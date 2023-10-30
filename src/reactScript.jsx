9e5e1e548fc1dc0e601f77a052f71071f13fa41f
Kaanoezki <135235440+Kaanoezki@users.noreply.github.com>
2023-10-30 12:11:14 +0100

    react-output test

--- src/reactScript.jsx
+++ src/reactScript.jsx
@@ -1,4 +1,12 @@
 //react testing
 import React from 'react';
-// import { render } from '@testing-library/react';
+import { ReactDOM } from 'react';
+class Gruss extends React.Component{
 
+    render(){
+        
+        return <h1>Hello world</h1>
+        
+    }
+};
+ReactDOM.render(<Gruss/>,document.getElementById('taskForm'));
